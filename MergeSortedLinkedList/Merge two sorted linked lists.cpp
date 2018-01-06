abb0e5a3d34cbf7c4f95fad14e3eda47ad8a4545
kushwahashiv <kushwaha.shiv@gmail.com>
2018-01-05 19:01:46 -0800

    update

--- MergeSortedLinkedList/Merge two sorted linked lists.cpp
+++ MergeSortedLinkedList/Merge two sorted linked lists.cpp
@@ -141,8 +141,7 @@ ListNode *merge2Lists(ListNode* l1, ListNode* l2) {
 	if (l1->val <= l2->val) {
 		l1->next = merge2Lists(l1->next, l2);
 		return l1;
-	}
-	else {
+	} else {
 		l2->next = merge2Lists(l1, l2->next);
 		return l2;
 	}
